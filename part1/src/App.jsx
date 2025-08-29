f3fe8ca3f6b558ed79cd41d9c87035102384ee67
hernanruscica <cesarhernanruscica@gmail.com>
2025-08-29 11:13:37 -0300

    Exercise 1.5: Course Information Step 5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -36,30 +36,30 @@ const Total = (props) => {
 
 const App = () => {
  
-  const course = 'Half Stack application development'
-
-  /*Exercise 1.4 : Course Information step 4  */
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises:10
-    },
-    {
-      name: "Using props to pass data",
-      exercises:7
-    },
-    {
-      name: "State of a component",
-      exercises:14
-    },
-  ]
-
+  /*Exercise 1.5 : Course Information step 5  */
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises:10
+      },
+      {
+        name: "Using props to pass data",
+        exercises:7
+      },
+      {
+        name: "State of a component",
+        exercises:14
+      },
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
