96ec4fe8316d1b2a0d59747cfd9d5c5befca21ca
hernanruscica <cesarhernanruscica@gmail.com>
2025-08-30 20:44:10 -0300

    1.12*: anecdotes step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -122,6 +122,41 @@ const StatisticLine = ({value, text, unit=''}) => {
   )
 }
 
+const Anecdotes = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]   
+  const [selected, setSelected] = useState(0);
+
+  const generateRandom = (min, max) => {
+    min = Math.ceil(min); 
+    max = Math.floor(max); 
+    return Math.floor(Math.random() * (max - min + 1)) + min;    
+  }
+
+  const handlerNextAnecdote = () => {
+    const nextAnecdote = generateRandom(0, anecdotes.length - 1);
+    setSelected(nextAnecdote);
+  }
+  
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      <p>
+        {anecdotes[selected]}
+      </p>
+      <MyButton btnText="next anecdote" handlerClick={handlerNextAnecdote}/>
+    </>
+  )
+}
+
 /* 1.8: unicafe step 3  */
 const Statistics = ({good, neutral, bad}) => {  
   const total = good + neutral + bad;
@@ -161,6 +196,7 @@ const App = () => {
         ? <Statistics good={good} neutral={neutral} bad={bad} />
         : <p>No feedback given</p>
       }
+      <Anecdotes />
     </div>
   )
 }
